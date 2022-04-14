5c45b0a5395fd47504037d223dd1af38f87d4cff
Matt Lee <mattlee@convene.com>
2022-04-13 20:42:02 -0400

    move NotesBoxHook code to Notesbox

--- src/CustomHooks/NotesBoxHook.js
+++ src/CustomHooks/NotesBoxHook.js
@@ -7,29 +7,6 @@ import { useRef, useState } from "react";
  * that copies the text in the textbox when the user hits the submit button.
  */
 
-const NotesBoxHook = () => {
-  const [inputNotes, setInputNotes] = useState("");
-  const textAreaRef = useRef(null);
-
-  const handleChangeNotes = (event) => {
-    setInputNotes(event.target.value);
-  };
-
-  const handleSubmitNotes = (event) => {
-    textAreaRef.current.select();
-    const copiedBoolean = document.execCommand("copy");
-    event.preventDefault();
-    if (copiedBoolean === true) {
-      window.alert("This note has been copied to the clipboard!");
-    }
-  };
-
-  return {
-    handleChangeNotes,
-    inputNotes,
-    handleSubmitNotes,
-    textAreaRef
-  };
-};
+const NotesBoxHook = () => {};
 
 export default NotesBoxHook;
--- src/Features/Notesbox.js
+++ src/Features/Notesbox.js
@@ -1,5 +1,5 @@
-import NotesBoxHook from "../CustomHooks/NotesBoxHook";
 import styles from "./Notesbox.module.css";
+import { useRef, useState } from "react";
 /**
  * This is a functional component of the notes textbox. A custom hook is imported to
  * handle updating the words displayed in the textbox and copying the text onto the user's
@@ -7,8 +7,21 @@ import styles from "./Notesbox.module.css";
  */
 
 const NotesBox = () => {
-  const { handleChangeNotes, inputNotes, handleSubmitNotes, textAreaRef } =
-    NotesBoxHook();
+  const [inputNotes, setInputNotes] = useState("");
+  const textAreaRef = useRef(null);
+
+  const handleChangeNotes = (event) => {
+    setInputNotes(event.target.value);
+  };
+
+  const handleSubmitNotes = (event) => {
+    textAreaRef.current.select();
+    const copiedBoolean = document.execCommand("copy");
+    event.preventDefault();
+    if (copiedBoolean === true) {
+      window.alert("This note has been copied to the clipboard!");
+    }
+  };
 
   return (
     <form onSubmit={handleSubmitNotes} className={styles.Notes_form}>
